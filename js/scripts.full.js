de8ac8ce184f9edc07320a2013c1e447a526b400
Anon <no@thanks.com>
2013-03-01 11:29:55 +0000

    b

--- js/scripts.full.js
+++ js/scripts.full.js
@@ -1,3 +1,5 @@
+var deviceID = "pc";
+
 $(document).ready(function()
 {
 	$( document ).bind( "mobileinit", function() {
@@ -7,5 +9,53 @@ $(document).ready(function()
 	});
 	
   // Custom code here!
-  
+	try
+	{
+		deviceID = device.uuid;
+	}
+	catch (err) { }
+		
+	console.log("THE DEVICE ID IS : "+deviceID);
 });
+
+function showMMC()
+{
+	$("#mmcimg").fadeIn(2500, function() { hideMMC(); });
+}
+
+function hideMMC()
+{
+	$("#mmcimg").fadeOut(2500, function() { showGameLogo(); });
+}
+
+function showGameLogo()
+{
+	$("#gameimg").fadeIn(2500, function() { hideGameLogo(); });
+}
+
+function hideGameLogo()
+{
+	$("#gameimg").fadeOut(2500, function() { finishLoad(); });
+}
+
+function finishLoad()
+{
+	$("#mmcimg").remove();
+	$("#gameimg").remove();
+
+	setTimeout(function() {
+	
+		$("#gameframe").ready(function () {
+			$("#loadingMessage").hide();
+			$("#gameframe").show();
+		});
+		
+		$("#gameframe").load(function () {
+			$("#loadingMessage").hide();
+			$("#gameframe").show();
+		});
+		
+		$("#gameframe").attr("src", "http://www.spygraph.org/game/menu.php?deviceID=" + deviceID);
+		
+	}, 2500);
+}
