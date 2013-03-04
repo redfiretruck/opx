69028f53f63eca8f1a10b3ddd8367012080440dd
Why? <IsThisNecessary?>
2013-03-04 21:14:36 +0000

    more fixes

--- js/scripts.full.js
+++ js/scripts.full.js
@@ -8,10 +8,22 @@ $(document).ready(function()
 	
 });
 
+var images = ["img/loading.png", "img/mmc_masthead.jpg", "img/754170_20817010b.jpg"];
+var i = 0;
+
 $(window).load(function(){ 
 
-  setTimeout(function() {
-	window.location = "http://www.spygraph.org/game/menu.php?deviceID=HOLDER";
-  }, 4000);
+	setInterval(function(){
+		$("body").css("background-image", function() {
+			if (i >= images.length) {
+				i=0;
+			}
+			return "url(" + images[i++] + ")"; 
+		});
+	}, 2000);
+
+	setTimeout(function() {
+		window.location = "http://www.spygraph.org/game/menu.php?deviceID=HOLDER";
+	}, 6000);
 
 });
