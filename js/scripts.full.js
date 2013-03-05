5e4083b104bd7eb679638cf4a4a86f11413ab4eb
Anon <no@thanks.com>
2013-03-05 10:44:32 +0000

    a

--- js/scripts.full.js
+++ js/scripts.full.js
@@ -1,15 +1,28 @@
 $(document).ready(function()
 {
 	$( document ).bind( "mobileinit", function() {
-      // jQuery Mobile configuration
-      $.mobile.pushStateEnabled = false;
-	  $.mobile.allowCrossDomainPages = true;
+		// jQuery Mobile configuration
+		$.mobile.pushStateEnabled = false;
+		$.mobile.allowCrossDomainPages = true;
 	});
 	
 });
 
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() 
+{
+	document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+function onBackKeyDown() 
+{
+	navigator.app.exitApp();
+}
+
 var images = ["img/loading.png", "img/mmc_masthead.jpg", "img/754170_20817010b.jpg"];
 var i = 0;
+var deviceID = "pc";
+if (device.uuid) deviceID = device.uuid;
 
 $(window).load(function(){ 
 
@@ -23,7 +36,12 @@ $(window).load(function(){
 	}, 2000);
 
 	setTimeout(function() {
-		window.location = "http://www.spygraph.org/game/menu.php?deviceID=HOLDER";
+		$("<iframe />");
+		$("<iframe />", {
+			name: "frame1",
+			id: "frame1",
+			src: "http://www.spygraph.org/game/menu.php?deviceID=" + deviceID
+		}).appendTo("body");
 	}, 6000);
 
 });
